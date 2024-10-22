0288dfa65da28896ea5993b17c7bf1315160e8ec
LucasCarvalho23 <lucasilva1990@hotmail.com>
2024-10-22 16:11:19 -0300

    Cooker Structure

---
+++ javascript/script.js
@@ -0,0 +1,19 @@
+class Cooker {
+    constructor() {
+        this.mouthOne = false
+        this.mouthTwo = false
+        this.mouthThree = false
+        this.mouthFour = false
+        this.stove = false
+        this.powerSocket = false
+        
+        console.log(this.mouthOne)
+        console.log(this.mouthTwo)
+        console.log(this.mouthThree)
+        console.log(this.mouthFour)
+        console.log(this.stove)
+        console.log(this.powerSocket)
+    }
+}
+
+const cooker = new Cooker()
