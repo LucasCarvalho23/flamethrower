f03185928c64e0dacfeede9662c1edecadff586e
LucasCarvalho23 <lucasilva1990@hotmail.com>
2024-10-22 16:48:36 -0300

    Mouth working

--- javascript/script.js
+++ javascript/script.js
@@ -1,18 +1,59 @@
 class Cooker {
     constructor() {
+        this.initialState()
+
+
+        this.powerSocketCooker = document.querySelector("#powerSocket")
+        this.powerSocketCooker.addEventListener("click", ()=> {
+            this.changePowerSocket()
+        })
+
+        this.mouthOneCooker = document.querySelector("#mouthOne")
+        this.mouthTwoCooker = document.querySelector("#mouthTwo")
+        this.mouthThreeCooker = document.querySelector("#mouthThree")
+        this.mouthFourCooker = document.querySelector("#mouthFour")
+
+        this.mouthOneCooker.addEventListener("click", ()=> {
+            this.changeState("mouthOne")           
+            this.exibe()
+        })
+        this.mouthTwoCooker.addEventListener("click", ()=> {
+            this.changeState("mouthTwo")           
+            this.exibe()
+        })
+        this.mouthThreeCooker.addEventListener("click", ()=> {
+            this.changeState("mouthThree")           
+            this.exibe()
+        })
+        this.mouthFourCooker.addEventListener("click", ()=> {
+            this.changeState("mouthFour")           
+            this.exibe()
+        })
+    }
+
+    initialState() {
         this.mouthOne = false
         this.mouthTwo = false
         this.mouthThree = false
         this.mouthFour = false
         this.stove = false
         this.powerSocket = false
-        
-        console.log(this.mouthOne)
-        console.log(this.mouthTwo)
-        console.log(this.mouthThree)
-        console.log(this.mouthFour)
-        console.log(this.stove)
-        console.log(this.powerSocket)
+    }
+
+    changePowerSocket() {
+        this.powerSocket = !this.powerSocket
+    }
+
+    changeState(mouth) {
+        if (!this.powerSocket) {
+            console.log("Tomada desligada. Ligue-a primeiramente")
+            return
+        }
+        this[mouth] = !this[mouth]
+    }
+
+    exibe() {
+        console.log(this.mouthOne, this.mouthTwo, this.mouthThree, this.mouthFour);
     }
 }
 
